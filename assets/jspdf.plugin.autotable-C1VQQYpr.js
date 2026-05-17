2155b26b9f327520236332ed981e07bca5ed4e81
todak2000 <todak2000@users.noreply.github.com>
2026-05-17 10:17:02 +0000

    deploy: 031cfe51cf51719494b7fe880ae3034a90e0b63b

--- assets/jspdf.plugin.autotable-C1VQQYpr.js
+++ assets/jspdf.plugin.autotable-C1VQQYpr.js
@@ -1,4 +1,4 @@
-import{g as V,a as q,c as J}from"./index-Bti4Qods.js";import{j as tt}from"./jspdf.es.min-LyL5yKl-.js";function et(Y,K){for(var N=0;N<K.length;N++){const L=K[N];if(typeof L!="string"&&!Array.isArray(L)){for(const E in L)if(E!=="default"&&!(E in Y)){const B=Object.getOwnPropertyDescriptor(L,E);B&&Object.defineProperty(Y,E,B.get?B:{enumerable:!0,get:()=>L[E]})}}}return Object.freeze(Object.defineProperty(Y,Symbol.toStringTag,{value:"Module"}))}var Z={exports:{}};const nt=V(tt);/*!
+import{g as V,a as q,c as J}from"./index-D3_XGD02.js";import{j as tt}from"./jspdf.es.min-D5ku4jvm.js";function et(Y,K){for(var N=0;N<K.length;N++){const L=K[N];if(typeof L!="string"&&!Array.isArray(L)){for(const E in L)if(E!=="default"&&!(E in Y)){const B=Object.getOwnPropertyDescriptor(L,E);B&&Object.defineProperty(Y,E,B.get?B:{enumerable:!0,get:()=>L[E]})}}}return Object.freeze(Object.defineProperty(Y,Symbol.toStringTag,{value:"Module"}))}var Z={exports:{}};const nt=V(tt);/*!
  * 
  *               jsPDF AutoTable plugin v3.8.4
  *
